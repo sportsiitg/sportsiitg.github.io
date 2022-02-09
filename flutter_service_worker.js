8f28b9b1631242ec15f91344f5c85e915e6f1a60
kjain333 <karanjain140701@gmail.com>
2022-02-09 09:38:36 +0530

    update

--- flutter_service_worker.js
+++ flutter_service_worker.js
@@ -15,9 +15,9 @@ const RESOURCES = {
 "favicon.ico": "7fb3d30e30fd9ba198f4a64237a2e0bd",
 "icons/Icon-192.png": "ac9a721a12bbc803b44f645561ecb1e1",
 "icons/Icon-512.png": "96e752610906ba2a93c65f8abe1645f1",
-"index.html": "57d8f4c7c0f7766a3d1eebeeb3d2f3ef",
-"/": "57d8f4c7c0f7766a3d1eebeeb3d2f3ef",
-"main.dart.js": "3ac89be80a87c4be74197d5a0a767860",
+"index.html": "32b181475efb7e468d577b38435b2376",
+"/": "32b181475efb7e468d577b38435b2376",
+"main.dart.js": "f5003c7907da8ab80a755a95aeafbd3a",
 "manifest.json": "79aa0533430b3fe573a1e2e404dbbdd7",
 "version.json": "2fd6b91cb2c6e8aa66ea32515093cb46"
 };
